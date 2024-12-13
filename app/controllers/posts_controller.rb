b3f8c4169fbd281a7d35ae03dc523fc98d4d6ccb
abdulQudoos7 <abdul.qudoos@southvillesol.com>
2024-12-13 19:22:18 +0500

    duplication test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+    @posts = Post.all
   end
 
   # GET /posts/1 or /posts/1.json
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    @post = Post.new
     @post = Post.new
   end
 
